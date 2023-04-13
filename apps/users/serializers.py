238b6b999174b8c6b42733b2b0f9b21f5050e0ad
Toktorov <ktoktorov2005@gmail.com>
2023-04-13 16:19:45 +0600

    add user register serializers

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -10,3 +10,44 @@ class UserSerializer(serializers.ModelSerializer):
                   'first_name', 'last_name', 'email',
                   'date_joined', 'profile_image', 'phone_number',
                   'verify')
+        
+class UserRegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=255, write_only=True
+    )
+    email = serializers.EmailField(
+        write_only=True
+    )
+    first_name = serializers.CharField(
+        max_length=255, write_only=True
+    )
+    last_name = serializers.CharField(
+        max_length=255, write_only=True
+    )
+    phone_number = serializers.CharField(
+        max_length=255, write_only=True
+    )
+    password = serializers.CharField(
+        max_length=255, write_only=True
+    )
+    password2 = serializers.CharField(
+        max_length=255, write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name',
+                  'phone_number', 'password', 'password2')
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            return serializers.ValidationError({'password' : 'Пароли отличаются'})
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
